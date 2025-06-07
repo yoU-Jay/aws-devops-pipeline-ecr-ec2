83c843d0c3df8679e9ddd3794424f80075c1e9a6
Ujwal Jagadeesh <59735375+yoU-Jay@users.noreply.github.com>
2025-06-07 18:43:17 +0530

    Feature/GitHub actions terraform (#15)
    * added github actions for terraform ecr plan
    
    * added github actions for terraform ecr plan
    
    * added github actions for terraform ecr plan
    
    * added github actions for terraform ecr plan
    
    * added github actions for terraform ecr plan
    
    * added github actions for terraform ecr plan

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,5 +1,5 @@
 module "ecr" {
-  source = "./module/ecr"
-  repo_name     = var.repo_name
-  scan_on_push  = var.scan_on_push
+  source       = "./module"
+  repo_name    = var.repo_name
+  scan_on_push = var.scan_on_push
 }
