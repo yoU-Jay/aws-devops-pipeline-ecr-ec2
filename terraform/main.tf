db5fbaf2b48085ad0a2e823316b2b1e8d34f0c88
Ujwal <ujjwal1141@gmail.com>
2025-06-08 11:58:56 +0530

    terraform fmt

--- terraform/main.tf
+++ terraform/main.tf
@@ -5,8 +5,8 @@ module "ecr" {
 }
 
 module "ec2" {
-  source         = "./module/ec2"
-  ami_id         = var.ami_id
-  instance_type  = var.instance_type
-  instance_name  = var.instance_name
+  source        = "./module/ec2"
+  ami_id        = var.ami_id
+  instance_type = var.instance_type
+  instance_name = var.instance_name
 }
