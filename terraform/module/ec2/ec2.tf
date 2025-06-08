89af26dba87610b02dfe486243c4fda59c2eba40
Ujwal <ujjwal1141@gmail.com>
2025-06-08 13:22:41 +0530

    terrform fmt

--- terraform/module/ec2/ec2.tf
+++ terraform/module/ec2/ec2.tf
@@ -1,6 +1,6 @@
 resource "aws_instance" "ec2_instance" {
-  ami           = var.ami_id
-  instance_type = var.instance_type
+  ami                    = var.ami_id
+  instance_type          = var.instance_type
   subnet_id              = var.subnet_id
   vpc_security_group_ids = [aws_security_group.this.id]
   key_name               = var.key_name
