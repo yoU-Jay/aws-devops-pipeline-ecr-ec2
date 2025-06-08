98ea6ea88cb017fc93232bc551ee6216da19d865
Sohan S Uppoor <sohan88.s.u@gmail.com>
2025-06-08 23:43:07 +0530

    updated user data

--- terraform/userdata.sh
+++ terraform/userdata.sh
@@ -1,7 +1,19 @@
 #!/bin/bash
+exec > /var/log/user-data.log 2>&1
+
 yum update -y
 amazon-linux-extras install docker -y
+yum install -y docker
 yum install -y dos2unix
+
 systemctl start docker
 systemctl enable docker
+
 usermod -a -G docker ec2-user
+
+# Confirm Docker is installed and running
+docker --version
+systemctl status docker
+
+# Add a marker file for confirmation
+echo "Docker installation complete" > /home/ec2-user/setup_done.txt
